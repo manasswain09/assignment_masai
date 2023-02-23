c8f07abd47316c3706a5fbffffd4a7add83d997b
manasswain09 <126001161+manasswain09@users.noreply.github.com>
2023-02-23 21:59:56 +0530

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,14 @@
+class Main1{
+public static void main(String [] args)
+{
+    String name="Manas";
+    String school="Masai school";
+    char grade='A';
+    char section ='A';
+    int rollno = 101;
+    int math=80;
+    int science=90;
+    int english=95;
+    System.out.println ("Name:"+name+"\nschool:"+school+ "\ngrade:"+grade+ "\nsection:"+section+"\nroll no :"+rollno+"\nmath:"+math+ "\nscience:"+science+ "\nenglish:"+english);
+}
+}
